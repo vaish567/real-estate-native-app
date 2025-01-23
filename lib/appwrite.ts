8a49af641d023b600ad9113af0f8d7ca7f1275e9
vaish567 <vaishnav.parte@gmail.com>
2025-01-23 19:29:42 +0530

    update

---
+++ lib/appwrite.ts
@@ -0,0 +1,23 @@
+import {
+    Client,
+    Account,
+    ID,
+    Databases,
+    OAuthProvider,
+    Avatars,
+    Query,
+    Storage,
+  } from "react-native-appwrite";
+
+export const config = {
+    platform: "com.jsm.restate",
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
